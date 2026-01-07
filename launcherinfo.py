2f17b2be9462e5a4be839f0b45b5682d72f4a058
MartinEesmaa <martin.eesmaa@protonmail.com>
2026-01-07 16:09:17 +0200

    Added launcherinfo for older RenPy versions build

---
+++ launcherinfo.py
@@ -0,0 +1,4 @@
+documentation_extensions = 'txt html md LICENSE'
+distribution_base = 'HAHCF-1.0.1'
+ignore_extensions = '~ .bak .mp4 .xcf .rpyc .rpyb progressive_download.txt *opening boy*.png girl*.png'
+executable_name = "Helga's Cheese Festival"
